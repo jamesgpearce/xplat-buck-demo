c0aae5c49ca323a68dfd3dfa044fa4dbcadd2d76
Shawn Wilsher <sdwilsh@fb.com>
2015-09-08 22:12:03 -0700

    Sample Android app stub
    This builds, but doesn't quite do what I want it to do yet.

---
+++ android/cpp/Hello.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <jni.h>
+#include "common/hello.h"
+
+jstring JNICALL
+Java_com_facebook_buck_demo_Hello_getHelloString(JNIEnv *env, jobject obj)
+{
+  const char *str = helloString();
+  return env->NewStringUTF(str);
+}
